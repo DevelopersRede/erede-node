39ced70feca135dab53a769c64c6856b753a06c4
igorp <igorpdasilvaa@gmail.com>
2019-10-08 21:43:21 -0300

    Refactoring for eslint

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
-"use strict";
-
-const Environment = require("./environment.js");
+const Environment = require('./environment.js');
 
 module.exports = class Store {
-    constructor(token, filiation, environment = Environment.production()) {
-        this.token = token;
-        this.filiation = filiation;
-        this.environment = environment;
-    }
+  constructor(token, filiation, environment = Environment.production()) {
+    this.token = token;
+    this.filiation = filiation;
+    this.environment = environment;
+  }
 };
