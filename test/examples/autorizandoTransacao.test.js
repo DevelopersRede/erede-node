2166f29556d811769ede6820d9277be52e422f29
igorp <igorpdasilvaa@gmail.com>
2019-10-24 20:28:18 -0300

    finalized all tests

--- test/examples/autorizandoTransacao.test.js
+++ test/examples/autorizandoTransacao.test.js
@@ -3,7 +3,7 @@ const Store = require('../../src/store');
 const Environment = require('../../src/environment');
 const Transaction = require('../../src/transaction');
 
-test('should authorize a transaction', () => {
+test('should authorize a transaction', async () => {
   const store = new Store(process.env.storeToken, process.env.storePV, Environment.sandbox());
   const transaction = new Transaction(10, Date.now()).creditCard(
     '5448280000000007',
@@ -13,7 +13,21 @@ test('should authorize a transaction', () => {
     'Fulano',
   );
 
-  return new ERede(store).create(transaction).then((resultOfTransaction) => {
-    expect(resultOfTransaction.returnCode).toBe('00');
-  });
-});
+  const resultOfTransaction = await new ERede(store).create(transaction);
+  expect(resultOfTransaction.returnCode).toBe('00');
+}, 50000);
+
+
+test('should authorize a transaction with installment', async () => {
+  const store = new Store(process.env.storeToken, process.env.storePV, Environment.sandbox());
+  const transaction = new Transaction(10, Date.now(), 2).creditCard(
+    '5448280000000007',
+    '235',
+    '12',
+    '2020',
+    'Fulano',
+  );
+
+  const resultOfTransaction = await new ERede(store).create(transaction);
+  expect(resultOfTransaction.returnCode).toBe('00');
+}, 50000);
