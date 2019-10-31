1f652dd2b60fe70c92e0543a6eb7babe12da3b34
Júlio César <julio.souzam@gmail.com>
2019-10-31 19:55:28 -0300

    configure eslint and prettier

--- __tests__/integration/transaction.test.js
+++ __tests__/integration/transaction.test.js
@@ -1,20 +1,20 @@
-require("dotenv").config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
-const eRede = require("../../lib/erede");
-const Transaction = require("../../lib/transaction");
-const store = require("../store");
+const eRede = require('../../lib/erede');
+const Transaction = require('../../lib/transaction');
+const store = require('../store');
 
-describe("Transactions", () => {
-  test("if user can create a transaction", async () => {
-    let transaction = new Transaction(
+describe('Transactions', () => {
+  test('if user can create a transaction', async () => {
+    const transaction = new Transaction(
       10,
       `ref${Math.ceil(Date.now())}`
-    ).creditCard("5448280000000007", "235", "12", "2020", "Fulano de Tal");
+    ).creditCard('5448280000000007', '235', '12', '2020', 'Fulano de Tal');
 
     const response = await new eRede(store).create(transaction);
 
-    expect(response.returnCode).toBe("00");
-  });
+    expect(response.returnCode).toBe('00');
+  }, 10000);
 });
